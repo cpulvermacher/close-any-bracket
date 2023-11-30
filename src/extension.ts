c0807677a3c2452c0974c462a376428d0e355f64
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2023-12-01 00:49:37 +0900

    basic close command

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,68 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	let close = vscode.commands.registerCommand('anybrace.close', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor)
+			return;
+
+		const cursorPosition = editor.selection.active;
+		const insertString = findNearestUnclosedParenthesis(editor.document, cursorPosition);
+
+		if (!insertString)
+			return;
+
+		editor.edit(editBuilder => {
+			editBuilder.insert(editor.selection.active, insertString);
+		});
+
+	});
+	context.subscriptions.push(close);
+}
+
+function findNearestUnclosedParenthesis(document: vscode.TextDocument, position: vscode.Position): string | null {
+	const lineText = document.lineAt(position.line).text.substr(0, position.character);
+
+	//work backward from cursor position
+	const closeCounts = {
+		parentheses: 0,
+		brackets: 0,
+		braces: 0,
+	};
+
+	for (let i = lineText.length - 1; i >= 0; i--) {
+		const char = lineText[i];
+
+		//TODO what if other counts are non-zero?
+		if (char === '(' && closeCounts.parentheses === 0) {
+			return ')';
+		}
+		if (char === '[' && closeCounts.brackets === 0) {
+			return ']';
+		}
+		if (char === '{' && closeCounts.braces === 0) {
+			return '}';
+		}
+
+		if (char === ')') {
+			closeCounts.parentheses++;
+		} else if (char === ']') {
+			closeCounts.brackets++;
+		} else if (char === '}') {
+			closeCounts.braces++;
+		} else if (char === '(') {
+			closeCounts.parentheses--;
+		} else if (char === '[') {
+			closeCounts.brackets--;
+		} else if (char === '{') {
+			closeCounts.braces--;
+		}
+	}
+	//TODO search more lines
+
+	return null;
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
