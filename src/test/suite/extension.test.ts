4a8795c4f9869518d484c5950e27001da7b0c2a1
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2023-12-15 00:32:33 +0900

    extract to util.ts

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,35 +3,9 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import { ClosingBracket, getBracketToInsert } from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	// vscode.window.showInformationMessage('Start all tests.');
 
 	//TODO proper test
-});
-
-suite('getBracketToInsert', () => {
-	test('closes open brackets', () => {
-		assert.strictEqual(getBracketToInsert('(', 1, 'javascript'), ')');
-		assert.strictEqual(getBracketToInsert('{', 1, 'javascript'), ')');
-		assert.strictEqual(getBracketToInsert('[', 1, 'javascript'), ')');
-	});
-
-	test('ignores closed brackets', () => {
-		assert.strictEqual(getBracketToInsert('([{}])', 0, 'javascript'), null);
-	});
-
-	test('ignores mismatched brackets if closed', () => {
-		assert.strictEqual(getBracketToInsert('[)]', 3, 'javascript'), ')');
-	});
-
-	test('weird: closes mismatched open brackets', () => {
-		assert.strictEqual(getBracketToInsert('[(]', 3, 'javascript'), ')');
-	});
-
-
-	test('broken: closes open brackets within strings', () => {
-		assert.strictEqual(getBracketToInsert('"{"', 3, 'javascript'), null);
-	});
 });
---
+++ src/util.ts
@@ -0,0 +1,188 @@
+import Prism from 'prismjs';
+import loadLanguages from 'prismjs/components/index';
+
+//load all languages
+loadLanguages();
+
+const BRACKET_CHARACTERS = new Set(["(", ")", "{", "}", "[", "]"]);
+
+export type ClosingBracket = ')' | ']' | '}';
+
+/** get bracket that should be closed at cursor position */
+export function getBracketToInsert(text: string, cursorOffset: number, languageId: string): ClosingBracket | null {
+    const grammar = getGrammar(languageId);
+    if (!grammar) {
+        console.log(`Couldn't find grammar for language ${languageId}`);
+        return null;
+    }
+
+    const allTokens = Prism.tokenize(text, grammar);
+    console.log("Tokenized input file:", allTokens);
+
+    const tokenBeforeCursor = getTokenBeforeOffset(allTokens, cursorOffset);
+    console.log(`Token before cursor`, tokenBeforeCursor);
+
+    //TODO there's a difference between 'before cursor' and 'surrounds cursor' (which is what context refers to)
+    // to get proper context, I'd want to 
+    const context = getContextAtCursor(allTokens, tokenBeforeCursor);
+
+    //TODO
+    // If we're inside a (non-template) string, fall back to dumb algorithm
+
+    const missing = getMissingBrackets(context, cursorOffset);
+    if (!missing) {
+        return null;
+    }
+
+    return missing[missing.length - 1];
+}
+
+
+/**
+ * Maps VSCode language identifiers (https://code.visualstudio.com/docs/languages/identifiers)
+ * to Grammar (https://prismjs.com/#supported-languages) or null if not found
+ */
+export function getGrammar(languageId: string): Prism.Grammar | null {
+
+    let grammarId: string;
+    switch (languageId) {
+        case 'javascriptreact':
+            grammarId = 'jsx';
+            break;
+        case 'typescriptreact':
+            grammarId = 'tsx';
+            break;
+        case 'objective-c':
+            grammarId = 'objectivec';
+            break;
+        case 'shellscript':
+            grammarId = 'shell';
+            break;
+        default:
+            grammarId = languageId;
+            break;
+    }
+
+    return Prism.languages[grammarId] || null;
+}
+
+/** Returns the list of tokens immediately before given offset.
+ * 
+ * A cursor offset of x represents a position _before_ x! E.g. for
+ * offset = 0, there are no tokens before 0, so this returns `null`.
+
+ * Usually this returns a single Token, but for a nested context
+ * (e.g. inside template strings) there may be multiple elements.
+ */
+export function getTokenBeforeOffset(tokens: Prism.TokenStream, cursorOffset: number): (string | Prism.Token)[] | null {
+    if (cursorOffset === 0) {
+        return null;
+    }
+
+    if (typeof tokens === 'string' || 'content' in tokens) {
+        // terminal token (string | Token)
+        return (cursorOffset < tokens.length) ? [tokens] : null;
+    }
+
+    let currentOffset = 0;
+    for (const token of tokens) {
+        //note: case for token starts after cursor cannot happen (cursorOffset > 0)
+
+        if (currentOffset + token.length < cursorOffset) {
+            // token ends before cursorOffset - 1, skip
+            currentOffset += token.length;
+        } else {
+            // token includes cursorOffset - 1
+            if (typeof token === 'string' || typeof token.content === 'string') {
+                return [token];
+            } else {
+                const childTokens = getTokenBeforeOffset(token.content, cursorOffset - currentOffset);
+                if (!childTokens) {
+                    return [token];
+                }
+                childTokens?.unshift(token);
+                return childTokens;
+            }
+        }
+    }
+
+    console.error("Couldn't find cursorOffset in tokens", tokens, cursorOffset);
+    return null;
+}
+
+/**
+ * use tokens at cursor offset to determine what our scope is.;
+ * this might be the entire file if we get only one(top - level) token,
+ * or e.g.a template string.*/
+export function getContextAtCursor(allTokens: (string | Prism.Token)[], tokensAtCursor: (string | Prism.Token)[] | null): Prism.TokenStream {
+    if (tokensAtCursor === null || tokensAtCursor.length <= 1) {
+        return allTokens;
+    }
+
+    //for tokensAtCursor.length >= 2, return the next level up
+    return tokensAtCursor[tokensAtCursor.length - 2];
+}
+
+/** Returns an array like ["}", ")"] with the brackets that are
+ * still unclosed at cursor position, or [] if balanced. */
+export function getMissingBrackets(tokens: Prism.TokenStream, cursorOffset: number): ClosingBracket[] {
+
+    if (typeof tokens === 'string' || 'type' in tokens) {
+        console.error(`Unexpected tokens type: ${typeof tokens}`);
+        if (typeof tokens !== 'string') {
+            return getMissingBrackets(tokens.content, cursorOffset);
+        }
+        return [];
+    }
+
+    // this assumes that all relevant tokens are on the top-level of `tokens`.
+    // (should be the case if using getContextAtCursor())
+    let expectedBrackets: ClosingBracket[] = [];
+    let currentOffset = 0;
+    for (const token of tokens) {
+        const bracketString = getBracketString(token);
+        if (bracketString) {
+            if (bracketString === '(') {
+                expectedBrackets.push(')');
+            } else if (bracketString === '[') {
+                expectedBrackets.push(']');
+            } else if (bracketString === '{') {
+                expectedBrackets.push('}');
+            } else if (bracketString === ')' || bracketString === ']' || bracketString === '}') {
+                if (expectedBrackets && expectedBrackets[expectedBrackets.length - 1] === bracketString) {
+                    expectedBrackets.pop();
+                } else {
+                    console.error(`Encountered unexpected bracket "${bracketString}", but expected last item in ${expectedBrackets}`);
+                    return [];
+                }
+            }
+
+        }
+        currentOffset += token.length;
+
+        //no need to check beyond cursor
+        if (currentOffset >= cursorOffset) {
+            break;
+        }
+    }
+    return expectedBrackets;
+}
+
+/** returns either a opening/closing bracket character if the token represents one, or null. */
+export function getBracketString(token: string | Prism.Token): string | null {
+    var tokenStr;
+    if (typeof token === 'string') {
+        tokenStr = token;
+    } else if (token.type === 'punctuation') {
+        tokenStr = token.content as string;
+    } else {
+        return null;
+    }
+
+    if (BRACKET_CHARACTERS.has(tokenStr)) {
+        return tokenStr;
+    } else {
+        return null;
+    }
+
+}
