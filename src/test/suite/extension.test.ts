d9cbae03fc1ce2f6e5faa8cb7b19248fea87019f
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2024-02-19 16:35:45 +0900

    integration test for extension commands

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,12 +1,73 @@
-import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import assert from 'assert';
 import * as vscode from 'vscode';
 
-suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
+const exampleJsFile = `
+if (abc) {
+    abc.def({
+`;
 
-    assert.strictEqual(1, 1);
-    //TODO proper test
+suite('Extension ', () => {
+    test('Closes single bracket in current doc', async () => {
+        const { waitForNewText } = await openEditor(
+            exampleJsFile,
+            'javascript',
+            3
+        );
+
+        await vscode.commands.executeCommand('close-any-bracket.close');
+
+        const text = await waitForNewText;
+        assert.strictEqual(text, exampleJsFile + '}');
+    });
+
+    test('Closes to indent in current doc', async () => {
+        const { waitForNewText } = await openEditor(
+            exampleJsFile,
+            'javascript',
+            3
+        );
+
+        await vscode.commands.executeCommand(
+            'close-any-bracket.close-to-indent'
+        );
+
+        const text = await waitForNewText;
+        assert.strictEqual(text, exampleJsFile + '})}\n');
+    });
+
+    async function openEditor(
+        content: string,
+        language: string,
+        lineNo: number
+    ) {
+        const document = await vscode.workspace.openTextDocument({
+            content,
+            language,
+        });
+        const position = new vscode.Position(lineNo, 0);
+        const selection = new vscode.Selection(position, position);
+        const editor = await vscode.window.showTextDocument(document, {
+            selection,
+        });
+        const waitForNewText = waitForTextChange(editor.document);
+
+        return { editor, waitForNewText };
+    }
+
+    /**
+     * text seems to be updated asynchronuously, so document.getText() may
+     * return old text until this event is triggered.
+     */
+    function waitForTextChange(document: vscode.TextDocument): Promise<string> {
+        return new Promise((resolve) => {
+            const disposable = vscode.workspace.onDidChangeTextDocument(
+                (ev) => {
+                    if (ev.document === document) {
+                        disposable.dispose();
+                        resolve(ev.document.getText());
+                    }
+                }
+            );
+        });
+    }
 });
