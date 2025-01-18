91487839e8dabd603042aebc2188be3d70127948
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-01-18 17:11:20 +0900

    fix: skip large languages without useful config in copy-languages.sh

--- scripts/copy-languages.sh
+++ scripts/copy-languages.sh
@@ -7,20 +7,24 @@
 # which introduces a very noticeable delay when using keybindings.
 for f in node_modules/prismjs/components/prism-*.min.js
 do
-    # via https://github.com/PrismJS/prism/blob/master/gulpfile.js/paths.js, the main prismjs bundle
-    # already includes the following language files, let's skip them
     case "$f" in
+        # via https://github.com/PrismJS/prism/blob/master/gulpfile.js/paths.js, the main prismjs bundle
+        # already includes the following language files, let's skip them
         *prism-core.min.js) continue ;;
         *prism-markup.min.js) continue ;;
         *prism-css.min.js) continue ;;
         *prism-clike.min.js) continue ;;
         *prism-javascript.min.js) continue ;;
+
+        # skip some large languages that don't make sense to include
+        *prism-apacheconf.min.js) continue ;; # no useful tokens
+        *prism-sqf.min.js) continue ;; # `clike` works just fine
     esac
     cp "$f" "out/$(basename "${f%.min.*}").js"
 done
 
 # check we have the expected number of language definitions
-EXPECTED_COUNT=293
+EXPECTED_COUNT=291
 ACTUAL_COUNT=$(ls out/prism-*.js | wc -l)
 if [ "$ACTUAL_COUNT" != "$EXPECTED_COUNT" ]; then
     echo "Error: Expected $EXPECTED_COUNT prism language definitions in out/, but found $ACTUAL_COUNT"
